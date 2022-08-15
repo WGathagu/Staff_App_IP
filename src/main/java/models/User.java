f1df78a0824c108081cd0bf12bf614a9a0624104
tots <rachealkari@gmail.com>
2022-08-15 15:43:31 +0300

    add user class

---
+++ src/main/java/models/User.java
@@ -0,0 +1,15 @@
+package models;
+
+public class User {
+    private String name;
+    private String phone;
+    private String email;
+    private int staffid;
+
+    public User(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+}
+
