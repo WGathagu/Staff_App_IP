a34fb49222f871b7b5ae7a904077e407115fe6ad
tots <rachealkari@gmail.com>
2022-08-15 15:52:49 +0300

    adding department and user properties

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,16 @@
 package models;
 
 public class Department {
+    private String departmentName;
+    private String departmentDescription;
+    private int noEmployees;
+    private int departmentid;
+    private int userId;
+
+    public Department(String departmentName, String departmentDescription, int noEmployees, int userId) {
+        this.departmentName = departmentName;
+        this.departmentDescription = departmentDescription;
+        this.noEmployees = noEmployees;
+        this.userId = userId;
+    }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -4,12 +4,17 @@ public class User {
     private String name;
     private String phone;
     private String email;
+    private String position;
+    private String department;
+
     private int staffid;
 
-    public User(String name, String phone, String email) {
+    public User(String name, String phone, String email, String position, String department) {
         this.name = name;
         this.phone = phone;
         this.email = email;
+        this.position = position;
+        this.department = department;
     }
 }
 
